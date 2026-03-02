02b3f23eaed1e224a0985bbce16a53d12fe9e8e5
Viranchi More <159642625+viru9192@users.noreply.github.com>
2026-03-01 22:45:40 -0500

    Add solution - CodeHub

---
+++ 6-kyu/multiplication-table/multiplication-table.py
@@ -0,0 +1,9 @@
+def multiplication_table(size):
+    table = []
+    for i in range(1, size+1):
+        row = []
+        for j in range(1, size+1):
+            row.append(i * j)
+        table.append(row)
+        
+    return table
