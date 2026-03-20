4101b251d4526e4baf34d2cf62c66a7cab4d1326
Viranchi More <159642625+viru9192@users.noreply.github.com>
2026-03-19 20:06:59 -0400

    Add solution - CodeHub

---
+++ 5-kyu/maximum-subarray-sum/maximum-subarray-sum.py
@@ -0,0 +1,9 @@
+def max_sequence(arr):
+    max_sum = 0
+    current_sum = 0
+​
+    for num in arr:
+        current_sum = max(0, current_sum + num)
+        max_sum = max(max_sum, current_sum)
+​
+    return max_sum
